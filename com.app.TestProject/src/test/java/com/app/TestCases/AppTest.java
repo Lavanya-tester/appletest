fb4a6931c9765b2461233f0c5d95e88439fa03c6
karthikmaley <50082882+karthikmaley@users.noreply.github.com>
2021-03-14 12:37:35 +0000

    First Commit

---
+++ com.app.TestProject/src/test/java/com/app/TestCases/AppTest.java
@@ -0,0 +1,19 @@
+package com.app.TestCases;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+   
+    @Test
+    public void verifytest()
+    
+    //how to run this project using jenkins
+    {
+       System.out.println("Selenium Test");
+       System.out.println("Java Selenium");
+       System.out.println("Python Test");
+       System.out.println("Ruby Test");
+       
+    }
+}
